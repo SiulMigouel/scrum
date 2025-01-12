1c77a2dd86ff5d18d48884f00c0a273762c626a2
La-Tva <144448452+La-Tva@users.noreply.github.com>
2025-01-12 18:07:19 +0100

    Add files via upload

---
+++ cours7.php
@@ -0,0 +1,49 @@
+<?php include('./header.html'); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./cours.css">
+    <title>cours 1</title>
+</head>
+<body>
+    
+<section class="cours">
+    <div class="container">
+
+        <div class="txt">
+        <h1>Les bonnes pratiques</h1>
+
+        <div class="contenue">
+            <p>
+                Certaines pratiques sont essentielles pour garantir que la méthode Scrum fonctionne efficacement.            </p>
+            <div class="section">
+            <h3>Communication ouverte :</h3>
+            <p>
+                La communication doit être fluide et transparente entre tous les membres de l’équipe et les parties extérieures : clients, responsables...            </p>
+            </div>
+            <div class="section">
+            <h3>Auto-organisation de l’équipe :</h3>
+            <p>
+                Les membres de l’équipe doivent être capables de s’organiser eux-mêmes pour accomplir les tâches sans supervision directe.            </p>
+            </div>
+            <div class="section">
+            <h3>Amélioration continue :</h3>
+            <p>
+                L’équipe doit toujours chercher à améliorer ses processus en se basant sur les retours obtenus lors des rétrospectives/ réunions.            </p>
+            </div>
+            <div class="boutons">
+                <a href="#">Me tester sur ce que je viens d'apprendre</a>
+                <a href="./cours8.php">Je continue le cours</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+</section>
+
+<?php include_once 'footer.html'; ?>
+</body>
+</html>
