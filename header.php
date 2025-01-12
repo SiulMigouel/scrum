c4f1cc0656d91348841554ac5629d54cea454fa2
La-Tva <144448452+La-Tva@users.noreply.github.com>
2025-01-12 22:53:35 +0100

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="nav.css">
-    <title>Navbar</title>
+    <title>SCRUM'MI apprend la méthode facilement</title>
 </head>
 <?php include 'popup_eval.php'; ?>
 <body>
