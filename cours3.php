86abbfdc777182de672518edce03c17e6fa6a866
SiulMigouel <luismiguel.meiraribeiro@gmail.com>
2025-01-13 15:10:35 +0100

    Update cours3.php

--- cours3.php
+++ cours3.php
@@ -48,7 +48,7 @@
         </div>
 
         <div class="img">
-            <video controls autoplay>
+            <video controls>
                 <source src="./images/video3.mp4" type="video/webm" />
               
                 <source src="./images/video3.mp4" type="video/mp4" />
